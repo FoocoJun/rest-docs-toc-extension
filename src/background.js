ef04edb16e60d9cb89c46973f07df207f1b6dba4
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2023-05-27 16:41:30 +0900

    [update] apply setIcon logic on background.js onMessage with docsDetected set popup with colored icon

--- src/background.js
+++ src/background.js
@@ -2,6 +2,15 @@
 
 chrome.runtime.onMessage.addListener((req, sender) => {
   if (sender.tab && req.docsDetected) {
+    // onMessage with docsDetected set popup with colored icon.
+    chrome.browserAction.setIcon({
+      tabId: sender.tab.id,
+      path: {
+        16: `icons/16.png`,
+        48: `icons/48.png`,
+        128: `icons/128.png`,
+      },
+    });
     // onMessage with docsDetected set popup with enabled.html.
     // enabled.html "RestDoc is detected on this page."
     chrome.browserAction.setPopup({
